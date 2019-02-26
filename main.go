af18356e783e6402ebf60b23c24b0cee110d768c
Kamil Marud <kamil.marud@gmail.com>
2019-02-26 19:52:42 +0100

    working version

--- main.go
+++ main.go
@@ -1,61 +1,52 @@
-// main.go
 package main
 
 import (
 	"fmt"
-	// "io"
 	"io/ioutil"
 	"os"
-
-	// "math"
 	"strconv"
 	"strings"
 )
 
 func main() {
-
 	filename := os.Args[1]
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "print: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error during reading file: %v\n", err)
 	}
-	var sumAll int
+	var sumOfAllAreas int
 	for _, line := range strings.Split(string(data), "\n") {
-
 		v := strings.Split(line, "x")
-		// fmt.Println(len(v))
+
 		if len(v) == 1 {
 			break
 		}
-		var i [3]int
-		i[0], _ = strconv.Atoi(v[0])
-		i[1], _ = strconv.Atoi(v[1])
-		i[2], _ = strconv.Atoi(v[2])
-		// math.Min(1, 2, 3)
-		mins := minim(i[0]*i[1], i[1]*i[2], i[0]*i[2])
-		// fmt.Println("mins", mins)
-		sum := i[0]*i[1]*2 + i[1]*i[2]*2 + i[0]*i[2]*2
-		sumAll += sum + mins
-		fmt.Println(v, sum, "+", mins, "=", (sum + mins))
+
+		i := []int{toInt(v[0]), toInt(v[1]), toInt(v[2])}
+
+		minValue := min(i[0]*i[1], i[1]*i[2], i[0]*i[2])
+		area := i[0]*i[1]*2 + i[1]*i[2]*2 + i[0]*i[2]*2
+		sumOfAllAreas += area + minValue
+		fmt.Printf("%v -> %d + %d = %d\n", v, area, minValue, (area + minValue))
 	}
 
-	fmt.Println("sum: ", sumAll)
+	fmt.Printf("You need %d dm^2 of paper\n", sumOfAllAreas)
 }
 
-func minim(args ...int) int {
-	// var mint int
-	mint := args[0]
+func toInt(s string) int {
+	v, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error during reading file: %v\n", err)
+	}
+	return v
+}
+
+func min(args ...int) int {
+	minValue := args[0]
 	for _, v := range args {
-		if v < mint {
-			mint = v
+		if v < minValue {
+			minValue = v
 		}
 	}
-	return mint
+	return minValue
 }
-
-// func m(a, b int) int {
-// 	if a <= b {
-// 		return a
-// 	}
-// 	return b
-// }
